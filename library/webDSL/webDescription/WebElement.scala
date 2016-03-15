7f1f05f899a1be45923e57d4fa9e834a803221d9
TDupriez <thomas.dupriez@epfl.ch>
2016-03-15 17:32:21 +0100

    Added a comment on WebElement

--- library/webDSL/webDescription/WebElement.scala
+++ library/webDSL/webDescription/WebElement.scala
@@ -3,7 +3,7 @@ package leon.webDSL.webDescription
 /**
   * Created by dupriez on 3/11/16.
   *
-  * All new subclasses of WebElement must also be registered in the pickler (see the companion object)
+  * All new subclasses of WebElement must also be registered in the pickler (see shared/src/main/scala/shared/Picklers (the "webElementPickler" implicit val))
   */
 sealed trait WebElement
 
