034a592101254247f210238693faa56a2ccf1067
TDupriez <thomas.dupriez@epfl.ch>
2016-03-24 15:43:28 +0100

    [Not working] Modification of the webDSL to support modifications from viewer to sourceCode

--- library/webDSL/webDescription/WebAttribute.scala
+++ library/webDSL/webDescription/WebAttribute.scala
@@ -2,7 +2,14 @@ package leon.webDSL.webDescription
 
 /**
   * Created by dupriez on 3/11/16.
+  * WebAttributes are to be seen as keys in a map that points to their corresponding value
   */
 sealed trait WebAttribute
+sealed trait StringWebAttribute extends WebAttribute
 
 case class TestWebAttribute1(oi: Int) extends WebAttribute
+
+case object Text extends StringWebAttribute
+
+//TODO: create a type of map for StringWebAttributes, which would know the default values for
+//TODO: these attributes and allow shorter code when the client unpack webElement to turn them into scalajs-react element
--- library/webDSL/webDescription/WebElement.scala
+++ library/webDSL/webDescription/WebElement.scala
@@ -6,10 +6,24 @@ package leon.webDSL.webDescription
   * All new subclasses of WebElement must also be registered in the pickler
   * (see shared/src/main/scala/shared/Picklers (the "webElementPickler" implicit val))
   */
-sealed trait WebElement
+sealed trait WebElement {
+  val id : WebElementID = WebElementIDProvider.generateFreshId()
+}
 
 //case class TestWebElement1(sons: leon.collection.List[WebElement]) extends WebElement
 //case class TestWebElement2(oi: Int) extends WebElement
 case class Div(sons: leon.collection.List[WebElement]) extends WebElement
-case class Header(level: HeaderLevel, text: String) extends WebElement
-case class Paragraph(text: String) extends WebElement
+case class Header(level: HeaderLevel, stringAttributes: leon.lang.Map[StringWebAttribute, String]) extends WebElement
+case class Paragraph(stringAttributes: leon.lang.Map[StringWebAttribute, String]) extends WebElement
+
+case class WebElementID(id: Int)
+object WebElementIDProvider {
+  private var counter = 0
+  def generateFreshId() = {
+    counter = counter + 1
+    WebElementID(counter)
+  }
+  def resetGenerator() = {
+    counter = 0
+  }
+}
